a4b2365cc1aef52f2b6b87456eb725c3dd9c97be
de-esteves <guilherme.esteves@grupoboticario.com.br>
2023-05-26 23:17:41 -0300

    feat: subindo pela branch de dev

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -6,7 +6,7 @@ provider "aws" {
 # Centralizar o arquivo de controle de estado do terraform
 terraform {
   backend "s3" {
-    bucket = "tf-state-igti-"
+    bucket = "tf-state-igti-${var.account}"
     key    = "state/igti/edc/mod1/exe1/terraform.tfstate"
     region = var.aws_region
   }
