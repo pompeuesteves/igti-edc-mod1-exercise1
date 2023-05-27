253530c2139b95d13b81148dc65e42fc2e4ce7f1
de-esteves <guilherme.esteves@grupoboticario.com.br>
2023-05-27 10:51:19 -0300

    feat: use of terraform features on client: init fmt validate plan

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,14 +1,18 @@
 resource "aws_s3_bucket" "dl" {
   bucket = "datalake-igti-edc-${var.account}-tf"
-  acl    = "private"
+  tags   = var.tags
+}
 
-  tags = var.tags
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.dl.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.dl.id
+  acl    = "private"
 }
